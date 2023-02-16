f803a377b03f579a0519940ff101db7a42f1befd
Phang Khan Dev <phangk4078@gmail.com>
2023-02-16 18:35:48 +0700

    hien thi danh sach Arr/obj

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,12 +2,28 @@ import React from "react";
 
 class DisplayInfor extends React.Component {
     render() {
-        const { age, name } = this.props;
+        const { listUsers } = this.props;
+        console.log(listUsers)
         //props => properties
         return (
             <div>
+                {listUsers.map((user) => {
+                    return (
+                        <div key={user.id}>
+                            <div>My name's {user.name}</div>
+                            <div>My age's {user.age}</div>
+                            <hr />
+                        </div>
+                    )
+                })}
+                {/* <div>My name's {name}</div>
+                <div>My age's {age}</div>
+                <hr />
                 <div>My name's {name}</div>
                 <div>My age's {age}</div>
+                <hr />
+                <div>My name's {name}</div>
+                <div>My age's {age}</div> */}
             </div>
         )
     }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,12 +3,23 @@ import DisplayInfor from "./DisplayInfor";
 import Userinfor from "./Userinfor";
 
 class MyComponent extends React.Component {
+    state = {
+        listUsers: [
+            { id: 1, name: "Phan Duong Khang", age: "22" },
+            { id: 2, name: "Dominic Phan", age: "23" },
+            { id: 3, name: "DomPhanDev", age: "24" },
+            { id: 4, name: "PhangKhanDev", age: "25" }
+        ]
+    }
+
     render() {
         return (
             <div>
-                <Userinfor></Userinfor>
+                <Userinfor />
                 <br /> <br />
-                <DisplayInfor name="Phan Duong Khang" age="18" />
+                <DisplayInfor
+                    listUsers={this.state.listUsers}
+                />
             </div>
 
         );
