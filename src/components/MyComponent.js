b32b231ab9d9b5dbfce05fbf730c36cfe8cd6f52
Phang Khan Dev <phangk4078@gmail.com>
2023-02-16 13:12:33 +0700

    setState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,7 +1,3 @@
-// có 2 cách định nghĩa component
-// 1. class component
-// 2. func component
-
 import React from "react";
 
 class MyComponent extends React.Component {
@@ -20,13 +16,29 @@ class MyComponent extends React.Component {
         })
     }
 
+    handleOnChang = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleOnSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state)
+    }
 
     render() {
         return (
 
             <div>
                 My name is {this.state.name} and I'm {this.state.age}
-                <button onClick={(event) => { this.handleClick(event) }}>Click me</button>
+                <form onSubmit={(event) => this.handleOnSubmit(event)}>
+                    <input
+                        type="text"
+                        onChange={(event) => this.handleOnChang(event)}
+                    />
+                    <button>Submit</button>
+                </form>
             </div>
 
         );
