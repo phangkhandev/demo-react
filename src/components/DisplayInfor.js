15842199fe372f87d7d0e6b1c6e93341f0fd3d12
Phang Khan Dev <phangk4078@gmail.com>
2023-02-18 12:49:44 +0700

    Vong doi ung dung

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -3,8 +3,23 @@ import './DisplayInfor.scss';
 import logo from './../logo.svg';
 
 class DisplayInfor extends React.Component {
-    state = {
-        isShowListUser: true
+    constructor(props) {
+        console.log('>>> call constructor')
+        super(props);
+        this.state = {
+            isShowListUser: true
+        }
+    }
+
+    componentDidMount() {
+        console.log('>>> call me component Did Mount')
+        setTimeout(() => {
+            document.title = 'test'
+        }, 3000);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('>>> call me component Did Update', this.props, prevProps)
     }
 
     handleShowHide = () => {
@@ -13,6 +28,7 @@ class DisplayInfor extends React.Component {
         })
     }
     render() {
+        console.log('>>> call me render ')
         const { listUsers } = this.props;
         return (
             <div className="display-ifnor-container">
