6a4ce5e75dc63458986cbf610c1cb514c2447720
Phang Khan Dev <phangk4078@gmail.com>
2023-02-20 23:53:28 +0700

    dieu huong trang voi router

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="home">PhangKhanDev</Navbar.Brand>
+                <Navbar.Brand href="/">PhangKhanDev</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
