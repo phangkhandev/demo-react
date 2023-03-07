d83c5348c64efdb092013c7b2f7592ae7f501ca6
Phang Khan Dev <phangk4078@gmail.com>
2023-03-07 16:04:21 +0700

    show list user

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -2,6 +2,7 @@ import ModalCreateUser from "./ModalCreateUser";
 import "./ManageUser.scss";
 import { FcPlus } from "react-icons/fc"
 import { useState } from "react";
+import TableUser from "./TableUser";
 
 const ManageUser = (props) => {
 
@@ -20,7 +21,7 @@ const ManageUser = (props) => {
                     </button>
                 </div>
                 <div className="table-users-container">
-                    table users
+                    <TableUser />
 
                 </div>
                 <ModalCreateUser
---
+++ src/components/Admin/Content/TableUser.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { getAllUsers } from "../../../services/apiService";
+
+const TableUser = (props) => {
+
+    const [listUsers, setListUsers] = useState([])
+
+    //componentDidmount
+    useEffect(() => {
+        fetchListUsers()
+    }, []);
+
+    const fetchListUsers = async () => {
+        let res = await getAllUsers();
+        if (res.EC === 0) {
+            setListUsers(res.DT)
+        }
+    }
+
+    return (
+        <>
+            <table className="table table-dark table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Username</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Role</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listUsers && listUsers.length > 0 &&
+                        listUsers.map((item, index) => {
+                            console.log('>>> check item: ', item)
+                            console.log('>>> check index: ', index)
+                            return (
+                                <tr key={`table-users-${index}`}>
+                                    <td>{index + 1}</td>
+                                    <td>{item.username}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.role}</td>
+                                    <td>
+                                        <button className="btn btn-primary">View</button>
+                                        <button className="btn btn-warning mx-3">Update</button>
+                                        <button className="btn btn-danger">Delete</button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+
+                    {listUsers && listUsers.length === 0 &&
+                        <tr>
+                            <td className="text-center" colSpan={4}>
+                                Not found data
+                            </td>
+                        </tr>}
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default TableUser;
