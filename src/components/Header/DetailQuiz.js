176011595b12fc02a57e04a4e2b4b3c3a8390427
Phang Khan Dev <phangk4078@gmail.com>
2023-03-21 16:20:01 +0700

    xu ly data

--- src/components/Header/DetailQuiz.js
+++ src/components/Header/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
+import _ from "lodash";
 
 const DetailQuiz = (props) => {
     const params = useParams();
@@ -14,6 +15,28 @@ const DetailQuiz = (props) => {
     const fetchQuestions = async () => {
         let res = await getDataQuiz(quizId);
         console.log(">>>check questions: ", res)
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                // Group the elements of Array based on `color` property
+                .groupBy("id")
+                // `key` is group's name (color), `value` is the array of objects
+                .map((value, key) => {
+                    let answers = [];
+                    let questionDiscription, image = null;
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDiscription = item.description;
+                            image = item.image;
+                        }
+                        answers.push(item.answers);
+                    })
+                    return { questionId: key, questionDiscription, image }
+                }
+                )
+                .value();
+            console.log(">>check data", data)
+        }
     }
 
     return (
