537bbe03d461a5113f8fc0d25ee32fd0e6f2a8b2
Phang Khan Dev <phangk4078@gmail.com>
2023-04-04 17:33:22 +0700

    base right content and css

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -5,6 +5,7 @@ import _ from "lodash";
 import './DetailQuiz.scss'
 import Question from "./Question";
 import ModalResult from "./ModalResult";
+import RightContent from "./Content/RightContent";
 
 const DetailQuiz = (props) => {
     const params = useParams();
@@ -157,7 +158,9 @@ const DetailQuiz = (props) => {
                 </div>
             </div>
             <div className="right-content">
-                count down
+                <RightContent
+                    dataQuiz={dataQuiz}
+                />
             </div>
 
             <ModalResult
--- src/components/User/Question.js
+++ src/components/User/Question.js
@@ -1,7 +1,10 @@
 import _ from "lodash";
+import { useState } from "react";
+import Lightbox from "react-awesome-lightbox";
 
 const Question = (props) => {
     const { data, index } = props;
+    const [isPreviewImage, setIsPreviewImage] = useState(false);
 
     if (_.isEmpty(data)) {
         return (<></>)
@@ -16,8 +19,21 @@ const Question = (props) => {
         <>
             {data.image ?
                 <div className="q-image">
-                    <img src={`data:image/jpeg;base64, ${data.image}`} />
-                </div>
+                    <img
+                        style={{ cursor: 'pointer' }}
+                        onClick={() => setIsPreviewImage(true)}
+                        src={`data:image/jpeg;base64, ${data.image}`}
+                    />
+                    {isPreviewImage === true &&
+                        <Lightbox
+                            image={`data:image/jpeg;base64, ${data.image}`}
+                            title={"Question Image"}
+                            onClose={() => setIsPreviewImage(false)}
+                        >
+
+                        </Lightbox>
+                    }
+                </div >
                 :
                 <div className="q-image">
 
