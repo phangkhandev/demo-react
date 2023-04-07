c9cd17038c0c8d876bf21d83126373227c06b9d5
Phang Khan Dev <phangk4078@gmail.com>
2023-04-07 16:04:41 +0700

    PrivateRoute

--- src/Layout.js
+++ src/Layout.js
@@ -13,6 +13,7 @@ import ListQuiz from './components/User/ListQuiz';
 import DetailQuiz from './components/User/DetailQuiz';
 import ManageQuiz from './components/Admin/Content/Quiz/ManageQuiz';
 import Questions from './components/Admin/Content/Question/Questions';
+import PrivateRoute from './routes/PrivateRoute';
 
 
 const NotFound = () => {
@@ -29,12 +30,24 @@ const Layout = (props) => {
             <Routes>
                 <Route path="/" element={<App />} >
                     <Route index element={<HomePage />} />
-                    <Route path="users" element={<ListQuiz />} />
+                    <Route path="users"
+                        element={
+                            <PrivateRoute>
+                                <ListQuiz />
+                            </PrivateRoute>
+                        }
+                    />
                 </Route>
 
                 <Route path="/quiz/:id" element={<DetailQuiz />} />
 
-                <Route path="/admin" element={<Admin />} >
+                <Route path="/admin"
+                    element={
+                        <PrivateRoute>
+                            <Admin />
+                        </PrivateRoute>
+                    }
+                >
                     <Route index element={<DashBoard />} />
                     <Route path="manage-users" element={<ManageUser />} />
                     <Route path="manage-quizzes" element={<ManageQuiz />} />
@@ -43,7 +56,6 @@ const Layout = (props) => {
 
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-
                 <Route path="*" element={<NotFound />} />
 
             </Routes>
